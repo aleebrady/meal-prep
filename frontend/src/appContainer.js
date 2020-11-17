79b4d5de6a6f8b615ebabf7f527c12721702c645
aleebrady <aleebrady@gmail.com>
2020-11-17 13:17:19 -0800

    appended elements to DOM

--- frontend/src/appContainer.js
+++ frontend/src/appContainer.js
@@ -10,10 +10,8 @@ class AppContainer {
     }
 
     getRandomMeals() {
-        debugger
         let dailyMeals = []
         for (let i = 0; i < 4; i++) {
-            debugger
             //change function to select a meal for each category, other than accross all categories
             dailyMeals.push(AppContainer.meals[Math.floor(Math.random()*AppContainer.meals.length)])
         }
@@ -22,23 +20,43 @@ class AppContainer {
 
     getMeals() {
         // fetch meals
-        console.log("testing")
         fetch(this.url + '/meals')
         .then(resp => resp.json())
             // populate the activities and categories properties with the returned data
         .then(data => {
+            console.log(data)
             data.forEach(meal => {
-                new Meal(meal.name)
+                new Meal(meal.name, meal.category)
             })
+            //call rendermeals
+            this.renderMeals()
         })
         // populate the activities and categories properties with the returned data
-        //call rendermeals
         .catch(error => alert(error))
     }
 
     renderMeals() {
         // create DOM nodes and insert data into them to render in DOM
-
+        const breakfastSelect = document.getElementById('breakfast');
+        const lunchSelect = document.getElementById('lunch');
+        const dinnerSelect = document.getElementById('dinner');
+        AppContainer.meals.forEach(meal => {
+            const option = document.createElement('option');
+            option.innerText = meal.name;
+            // append will be conditional based on what category it belongs to
+            // debugger
+            switch(meal.category.name) {
+                case "Breakfast":
+                    breakfastSelect.appendChild(option);
+                  break;
+                case "Lunch":
+                    lunchSelect.appendChild(option);
+                  break;
+                case "Dinner":
+                  dinnerSelect.appendChild(option);
+                 break;
+              }
+        })
     }
 
 }
--- frontend/src/meal.js
+++ frontend/src/meal.js
@@ -1,6 +1,7 @@
 class Meal {
-    constructor(name) {
+    constructor(name, category) {
         this.name = name
+        this.category = category
         AppContainer.meals.push(this)
     }
 }
