d7c86092ec2812e39d6dfc9e79b121db92919ddc
aleebrady <aleebrady@gmail.com>
2020-11-18 22:59:52 -0800

    added network messages

--- meal-prep-api/app/controllers/meals_controller.rb
+++ meal-prep-api/app/controllers/meals_controller.rb
@@ -2,18 +2,18 @@ class MealsController < ApplicationController
 
     def index
         @meals = Meal.all 
-        render json: @meals, :include => :category
+        render json: @meals, :include => :category, :status => 200
     end
 
     def create
         category = Category.find_by(name: params[:category].downcase)
         meal = Meal.create(name: params[:name], category: category)
-        render :json => meal
+        render :json => meal, :status => 201
     end
 
     def destroy
         @meal = Meal.find(params[:id]).destroy
-        render :json => {id: params[:id], message: "meal was deleted"}
+        render :json => {id: params[:id], message: "meal was deleted"}, :status => 200
         # sqlite3 errors
         # if @meal.destroy
         #     render :json => {id: params[:id], message: "meal was deleted"}
